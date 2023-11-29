67e4c9e22f8abf43eb046b4d548781af0dca9399
EstebanBel29 <estebanbeltran29@gmail.com>
2023-11-29 17:03:09 -0300

    Se puso el video de background

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import { Link } from 'react-router-dom';
 //components
 
-
 function App() {
   
   return (
     <div className='home-container'>
-      <h1>Movie App</h1>
-      <h3 className='background-color'>Welcome to your movie gallery</h3>
-      <p>In this app you can add, delete and edit the movies you have seen</p>
-      <Link to="/login" className='start-button'>Lets go!</Link>
+      <video className='video' src="./assets/backgroundVideo.mp4" autoPlay loop muted/>
+      <div className='text-container'>
+        <h1>Movie App</h1>
+        <h3 className='background-color'>Welcome to your movie gallery</h3>
+        <p>In this app you can add, delete and edit the movies you have seen</p>
+        <Link to="/login" className='start-button'>Lets go!</Link>
+      </div>
     </div>
   );
 }
